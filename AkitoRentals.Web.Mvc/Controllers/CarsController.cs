1be2c38b4546a5ac3ed364a048745015ef5bc0d2
TelerikGeek <shermanchen@theprogrammingnerd.com>
2016-04-28 16:52:19 +0800

    Updated the CarsController class file.

--- AkitoRentals.Web.Mvc/Controllers/CarsController.cs
+++ AkitoRentals.Web.Mvc/Controllers/CarsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -52,6 +53,99 @@ namespace AkitoRentals.Web.Mvc.Controllers
 			ViewBag.CarRecords = carResults;
 
 			return View ( );
+		}
+
+		[HttpPost]
+		public ActionResult Create ( CarViewModel car )
+		{
+			var newCar = new Car ( )
+			{
+				VehicleNo = car.VehicleNo,
+				RentalCarCode = car.RentalCarCode,
+				TypeId = car.TypeId,
+				ModelId = car.ModelId,
+				MakeId = car.MakeId,
+				AvailabilityId = car.AvailabilityId,
+				YearsOfUse = car.YearsOfUse,
+				MonthsOfUse = car.MonthsOfUse,
+				SeatingCapacity = car.SeatingCapacity,
+				DateCreated = DateTime.Now,
+				DateModified = DateTime.Now,
+				StatusId = car.StatusId
+			};
+
+			context.Cars.Add ( newCar );
+			context.SaveChanges ( );
+
+			return View ( newCar );
+		}
+
+		[HttpGet]
+		public ActionResult UpdateRentalCarCode()
+		{
+			string rentalCarCode = Request.QueryString [ "rentalCode" ];
+			int carId = Convert.ToInt32 ( Request.QueryString [ "carId" ] );
+
+			var carToUpdate = context.Cars.Single ( c => c.CarId == carId );
+			carToUpdate.RentalCarCode = rentalCarCode;
+			carToUpdate.DateModified = DateTime.Now;
+
+			context.Entry ( carToUpdate ).State = EntityState.Modified;
+			context.Cars.Attach ( carToUpdate );
+			context.SaveChanges ( );
+
+			return View ( carToUpdate );
+		}
+
+		[HttpGet]
+		public ActionResult UpdateVehicleNo()
+		{
+			string vehicleNo = Request.QueryString [ "vehicleNo" ];
+			int carId = Convert.ToInt32 ( Request.QueryString [ "carId" ] );
+
+			var carToUpdate = context.Cars.Single ( c => c.CarId == carId );
+			carToUpdate.VehicleNo = vehicleNo;
+			carToUpdate.DateModified = DateTime.Now;
+
+			context.Entry ( carToUpdate ).State = EntityState.Modified;
+			context.Cars.Attach ( carToUpdate );
+			context.SaveChanges ( );
+
+			return View ( carToUpdate );
+		}
+
+		[HttpGet]
+		public ActionResult UpdateCarType()
+		{
+			int typeId = Convert.ToInt32 ( Request.QueryString [ "typeId" ] );
+			int carId = Convert.ToInt32 ( Request.QueryString [ "carId" ] );
+
+			var carToUpdate = context.Cars.Single ( c => c.CarId == carId );
+			carToUpdate.TypeId = typeId;
+			carToUpdate.DateModified = DateTime.Now;
+
+			context.Entry ( carToUpdate ).State = EntityState.Modified;
+			context.Cars.Attach ( carToUpdate );
+			context.SaveChanges ( );
+
+			return View ( carToUpdate );
+		}
+
+		[HttpGet]
+		public ActionResult UpdateCarModel ( )
+		{
+			int modelId = Convert.ToInt32 ( Request.QueryString [ "modelId" ] );
+			int carId = Convert.ToInt32 ( Request.QueryString [ "carId" ] );
+
+			var carToUpdate = context.Cars.Single ( c => c.CarId == carId );
+			carToUpdate.ModelId = modelId;
+			carToUpdate.DateModified = DateTime.Now;
+
+			context.Entry ( carToUpdate ).State = EntityState.Modified;
+			context.Cars.Attach ( carToUpdate );
+			context.SaveChanges ( );
+
+			return View ( carToUpdate );
 		}
     }
 }
